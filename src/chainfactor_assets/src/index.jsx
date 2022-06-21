4bf35faf93dc19ae1cb401b822b4d096fbd6a4d7
sspc <mus56789@gmail.com>
2022-06-21 00:10:35 -0400

    Update index.jsx

--- src/chainfactor_assets/src/index.jsx
+++ src/chainfactor_assets/src/index.jsx
@@ -2,41 +2,26 @@ import * as React from "react";
 import { render } from "react-dom";
 import { chainfactor } from "../../declarations/chainfactor";
 
-// import Create from '../../invoice_assets/components/create.jsx';
-// import Read from '../../invoice_assets/components/read.jsx';
-// import Update from '../../invoice_assets/components/update.jsx';
-// import Delete from '../../invoice_assets/components/delete.jsx';
 import Create from '../components/create.jsx';
 import Read from '../components/read.jsx';
-import Update from '../components/update.jsx';
 import Delete from '../components/delete.jsx';
 
 const MyHello = () => {
 
   const [name, setName] = React.useState('');
   const [message, setMessage] = React.useState('');
- 
-  async function doGreet() {
-    const greeting = await chainfactor.greet(name);
-    setMessage(greeting);
-  } 
-
-  async function doReadText() {
-    const readText = await chainfactor.readText(name);
-    setMessage(readText);
-  } 
 
   async function doCreateNFT() {
     const createNFT = await chainfactor.createNFT( parseInt(name));
     setMessage(createNFT);
-  } 
-  
+  }
+
   async function doCreateDAO() {
     const balNFT = await chainfactor.createDAO();
     setMessage(balNFT);
   }
 
- 
+
 
   return (
     <div style={{ "fontSize": "20" }}>
@@ -44,13 +29,6 @@ const MyHello = () => {
         <p><h3>ChainFactor</h3></p>
       </div>
 
-      <div style={{ "backgroundColor": "lightyellow" }}>
-        <p>
-          {" "}
-          Invoice financing allows you to borrow against your outstanding invoices. <br/> 
-          With factoring, you're selling your invoices to a factoring company at a discount.
-        </p>
-      </div>
 
       <div style={{ margin: "30px" }}>
         <input
@@ -58,19 +36,8 @@ const MyHello = () => {
           value={name}
           onChange={(ev) => setName(ev.target.value)}
         ></input>
-        <button onClick={doGreet}>Get Greeting!</button>
-        Greeting is: "
-        <span style={{ color: "blue" }}>{message}</span>"
-      </div>
-
-      <div style={{ margin: "30px" }}>
-        <input
-          id="name"
-          value={name}
-          onChange={(ev) => setName(ev.target.value)}
-        ></input>
-        <button onClick={doReadText}>ReadText!</button>
-        readText is: "
+        <button onClick={doCreateDAO}>createDAO!</button>
+        createDAO is: "
         <span style={{ color: "blue" }}>{message}</span>"
       </div>
 
@@ -84,8 +51,8 @@ const MyHello = () => {
         createNFT is: "
         <span style={{ color: "blue" }}>{message}</span>"
       </div>
-      
-      
+
+
       <div style={{ "backgroundColor": "lightblue" }}>
         <p>
            <h3>Enter Invoice</h3>
